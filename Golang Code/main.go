60e209c658de2d03a5e542b6281bd8ccbd2e892c
Vasin Kirill <vukivur@gmail.com>
2024-11-24 18:16:36 +0300

    operator switch-case

--- Golang Code/main.go
+++ Golang Code/main.go
@@ -66,4 +66,14 @@ func main() {
 		fmt.Println("Unknown number")
 	}
 
+	var c uint32 = 5100
+
+	switch {
+	case 1 <= c && c <= 9:
+		fmt.Println("От  до 9")
+	case 100 <= c && c <= 250:
+		fmt.Println("От 100 до 250")
+	case 1000 <= c && c <= 6000:
+		fmt.Println("От 1000 до 6000")
+	}
 }
